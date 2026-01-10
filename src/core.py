34807a7be20386e649547c0d10e1ddad88009398
Kyle Patterson <kylekaptbone@yahoo.com>
2026-01-09 16:14:26 -0800

    Add initial implementations for two sum and add two numbers solutions

--- src/core.py
+++ src/core.py
@@ -1,25 +1,20 @@
-import logging
-
-logging.basicConfig(
-    filename="reports/all.log",
-    encoding="utf-8",
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    level=logging.INFO,
-)  # put this only in main core.py file
-
-logger = logging.getLogger(__name__)  # put this in each file
+import time
 
 
-def main():
-    # Main function
-
-    try:
-        True  # noqa: B018
-    except Exception as E:
-        logger.warning(type(E).__name__, __file__, E.__traceback__.tb_lineno, "\n", E)
-        raise
+def problem3(s):
+    max_val = 0
+    if s == "":
+        return 0
+    if len(s) == len(set(s)):
+        return len(s)
+    for i in range(len(s)):
+        for j in range(i + 1 + max_val, len(s)+1):
+            if len(set(s[i:j])) == len(s[i:j]):
+                max_val = max(max_val, len(s[i:j]))
+    return max_val
 
 
 if __name__ == "__main__":
     """[summary]"""
-    main()
+    start_time = time.time()
+    print(problem3("mpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuztt"), time.time() - start_time)
